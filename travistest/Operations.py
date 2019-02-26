d0262ddf0493566fe87b594844c4fa552899b580
ritesh3105 <ritesh.d@amazatic.com>
2019-02-26 16:08:29 +0530

    initial commit.

---
+++ travistest/Operations.py
@@ -0,0 +1,16 @@
+class Operations:
+    def __init__(self):
+        print('inside operations')
+        pass
+    
+    def add(self,x,y):
+        return x + y
+
+    def subtract(self,x,y):
+        return x - y
+
+    def increment(self,x):
+        return x + 1
+
+    def decrement(self,x):
+    	return x - 1
